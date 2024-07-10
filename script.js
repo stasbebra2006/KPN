a2d4329053564557adb89b668a1c0f569c0aef52
Stanislav Liashevskyi <liashevskiys@gmail.com>
2024-07-10 19:21:59 +0200

    Revised version

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const choices = ['K', 'P', 'N'];
 let userScore = 0;
 let computerScore = 0;
+let userChoice;
+let computerChoice;
 let gameHistory = [];
 let choiceCount = { 'K': 0, 'P': 0, 'N': 0 };
 const winningCombinations = {
@@ -8,20 +11,19 @@ const winningCombinations = {
 	'N': 'P'
 };
 
-function startGame() {
+document.addEventListener('keydown', function (event) {
+	if (event.key === ' ') startGame();
+});
+
+async function startGame() {
 	alert("Hra začíná! Zadejte K pro Kámen, P pro Papír nebo N pro Nůžky.");
-	const userChoice = prompt("Zadejte K (Kámen), P (Papír) nebo N (Nůžky):").toUpperCase();
-	const choices = ['K', 'P', 'N'];
-	const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-	choiceCount[userChoice]++;
-	choiceCount[computerChoice]++;
-	console.log(`Uživatel: ${userChoice}, Počítač: ${computerChoice}`);
-	determineWinner(userChoice, computerChoice);
-	updateScoreboard();
+	getChoices()
+	await determineWinner(userChoice, computerChoice);
+	showTheWinner();
 	gameHistory.push({ userChoice, computerChoice, userScore, computerScore });
 }
 
-function determineWinner(userChoice, computerChoice) {
+async function determineWinner(userChoice, computerChoice) {
 	if (userChoice === computerChoice) {
 		alert("Je to remíza!");
 		console.log("Je to remíza!");
@@ -29,15 +31,50 @@ function determineWinner(userChoice, computerChoice) {
 		alert("Vyhráli jste!");
 		console.log("Uživatel vyhrává!");
 		userScore++;
+		await updateScoreboard();
+		await updateChoiceCount();
 	} else {
 		alert("Počítač vyhrává!");
 		console.log("Počítač vyhrává!");
 		computerScore++;
+		await updateScoreboard();
+		await updateChoiceCount();
 	}
 }
 
 function updateScoreboard() {
 	document.getElementById("userScore").textContent = `Skóre Uživatele: ${userScore}`;
 	document.getElementById("computerScore").textContent = `Skóre Počítače: ${computerScore}`;
+}
+
+async function showTheWinner() {
+	await updateScoreboard();
+	if (userScore === 5 || computerScore === 5) {
+		alert(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`);
+		console.log(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`);
+		console.log(gameHistory);
+		restartGameVariables();
+	}
+}
+
+function getChoices() {
+	do {
+		userChoice = prompt("Zadejte K (Kámen), P (Papír) nebo N (Nůžky):").toUpperCase().replace(/\s+/g, '');
+	} while (!['K', 'P', 'N'].includes(userChoice));
+	computerChoice = choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updateChoiceCount() {
+	choiceCount[userChoice]++;
+	choiceCount[computerChoice]++;
+	console.log(`Uživatel: ${userChoice}, Počítač: ${computerChoice}`);
 	console.log(`K: ${choiceCount['K']}, P: ${choiceCount['P']}, N: ${choiceCount['N']}`);
+}
+
+function restartGameVariables() {
+	userScore = 0;
+	computerScore = 0;
+	updateScoreboard();
+	gameHistory = [];
+	choiceCount = { 'K': 0, 'P': 0, 'N': 0 };
 }
