2e08c9bab5b290f73d70640fedc552d36bf9573e
Stas <bububa>
2024-07-13 05:19:51 +0200

    polishing

--- script.js
+++ script.js
@@ -1,93 +1,95 @@
-const choices = ['K', 'P', 'N'];
-let userScore = 0;
-let computerScore = 0;
-let userChoice;
-let computerChoice;
-let gameHistory = [];
-let choiceCount = { 'K': 0, 'P': 0, 'N': 0 };
+const choices = ['K', 'P', 'N']
+const userScoreCounter = document.getElementById("userScore")
+const computerScoreCounter = document.getElementById("computerScore")
+const outcomes = {
+	'draw': (userChoice, computerChoice) => userChoice === computerChoice,
+	'userWins': (userChoice, computerChoice) => winningCombinations[userChoice] === computerChoice,
+	'computerWins': (userChoice, computerChoice) => winningCombinations[userChoice] !== computerChoice
+}
 const winningCombinations = {
 	'K': 'N',
 	'P': 'K',
 	'N': 'P'
 };
+let userScore = 0
+let computerScore = 0
+let userChoice
+let computerChoice
+let gameHistory = []
+let choiceCount = { 'K': 0, 'P': 0, 'N': 0 }
 
 document.addEventListener('keydown', function (event) {
-	if (event.key === ' ') startGame();
+	if (event.key === ' ') startGame()
 });
 
 function startGame() {
-	alert("Hra začíná! Zadejte K pro Kámen, P pro Papír nebo N pro Nůžky.");
+	alert("Hra začíná! Zadejte K pro Kámen, P pro Papír nebo N pro Nůžky.")
 	getChoices()
-	determineWinner();
-	gameHistory.push({ userChoice, computerChoice, userScore, computerScore });
+	determineWinner()
+	gameHistory.push({ userChoice, computerChoice, userScore, computerScore })
 }
 
 function determineWinner() {
-	let outcome = outCome(userChoice, computerChoice);
+	let outcome = outCome(userChoice, computerChoice)
 	switch (outcome) {
 		case 'draw':
-			alert("Je to remíza!");
-			console.log("Je to remíza!");
+			alert("Je to remíza!")
+			console.log("Je to remíza!")
 			break;
 		case 'userWins':
-			alert("Vyhráli jste!");
-			console.log("Uživatel vyhrává!");
+			alert("Vyhráli jste!")
+			console.log("Uživatel vyhrává!")
 			userScore++;
-			updateScoreboard();
-			updateChoiceCount();
-			if (userScore === 5 || computerScore === 5) {
-				setTimeout(showTheWinner, 100);
-			}
 			break;
 		case 'computerWins':
-			alert("Počítač vyhrává!");
-			console.log("Počítač vyhrává!");
-			computerScore++;
-			updateScoreboard();
-			updateChoiceCount();
-			if (userScore === 5 || computerScore === 5) {
-				setTimeout(showTheWinner, 100);
-			}
+			alert("Počítač vyhrává!")
+			console.log("Počítač vyhrává!")
+			computerScore++
 			break;
+	}
+	updateScoreboard()
+	updateChoiceCount()
+	if (userScore === 5 || computerScore === 5) {
+		setTimeout(showTheWinner, 100);
 	}
 }
 
 function updateScoreboard() {
-	document.getElementById("userScore").textContent = `Skóre Uživatele: ${userScore}`;
-	document.getElementById("computerScore").textContent = `Skóre Počítače: ${computerScore}`;
+	userScoreCounter.textContent = `Skóre Uživatele: ${userScore}`
+	computerScoreCounter.textContent = `Skóre Počítače: ${computerScore}`
 }
 
 async function showTheWinner() {
-	await updateScoreboard();
-	alert(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`);
-	console.log(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`);
-	console.log(gameHistory);
-	restartGameVariables();
+	await updateScoreboard()
+	alert(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`)
+	console.log(`Konec hry! ${userScore === 5 ? 'Uživatel' : 'Počítač'} vyhrál!`)
+	console.log(gameHistory)
+	restartGameVariables()
 }
 
 function getChoices() {
 	do {
-		userChoice = prompt("Zadejte K (Kámen), P (Papír) nebo N (Nůžky):").toUpperCase().replace(/\s+/g, '');
-	} while (!['K', 'P', 'N'].includes(userChoice));
-	computerChoice = choices[Math.floor(Math.random() * choices.length)];
+		let input = prompt("Zadejte K (Kámen), P (Papír) nebo N (Nůžky):");
+		userChoice = input ? input.toUpperCase().replace(/\s+/g, '') : null;
+	} while (!['K', 'P', 'N'].includes(userChoice))
+	computerChoice = choices[Math.floor(Math.random() * choices.length)]
 }
 
 function updateChoiceCount() {
-	choiceCount[userChoice]++;
-	choiceCount[computerChoice]++;
-	console.log(`Uživatel: ${userChoice}, Počítač: ${computerChoice}`);
-	console.log(`K: ${choiceCount['K']}, P: ${choiceCount['P']}, N: ${choiceCount['N']}`);
+	choiceCount[userChoice]++
+	choiceCount[computerChoice]++
+	console.log(`Uživatel: ${userChoice}, Počítač: ${computerChoice}`)
+	console.log(`K: ${choiceCount['K']}, P: ${choiceCount['P']}, N: ${choiceCount['N']}`)
 }
 
 function restartGameVariables() {
-	userScore = 0;
-	computerScore = 0;
-	updateScoreboard();
-	gameHistory = [];
-	choiceCount = { 'K': 0, 'P': 0, 'N': 0 };
+	userScore = 0
+	computerScore = 0
+	updateScoreboard()
+	gameHistory = []
+	choiceCount = { 'K': 0, 'P': 0, 'N': 0 }
 }
 
 function outCome(userChoice, computerChoice) {
-	return userChoice === computerChoice ? 'draw' :
-		winningCombinations[userChoice] === computerChoice ? 'userWins' : 'computerWins';
+	return Object.keys(outcomes).find(outcome => outcomes[outcome](userChoice, computerChoice))
 }
